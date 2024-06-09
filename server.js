461d061c4b369658fd0c503573acaaba2f58ec80
Hariharaan-S <138239947+Hariharaan-S@users.noreply.github.com>
2024-06-09 21:23:27 +0530

    Commit Changes

---
+++ server.js
@@ -0,0 +1,124 @@
+import express from 'express'
+import bodyParser from 'body-parser';
+import cors from 'cors'
+import mongoose from 'mongoose';
+import { v4 as uuidv4 } from 'uuid';
+
+mongoose.connect("mongodb://localhost:27017/taskmanagement")
+
+const ProfileSchema = new mongoose.Schema(
+    {
+        id: String,
+        displayName: String,
+        email: String,
+        password: String
+    }
+)
+
+const TaskSchema = new mongoose.Schema(
+    {
+        id: String,
+        title: String,
+        description: String,
+        due_date: Number
+    }
+)
+
+const PastTaskSchema = new mongoose.Schema(
+    {
+        id: String,
+        title: String,
+        description: String,
+        due_date: Number
+    }
+)
+
+const Profile = new mongoose.model('profile', ProfileSchema)
+const Task = new mongoose.model('task', TaskSchema)
+const PastTask = new mongoose.model('pasttask', PastTaskSchema)
+const app = express();
+
+app.use(cors());
+
+app.use(bodyParser.json())
+
+app.post('/auth/register', (req, res) => {
+    var profile = req.body
+    const newId = uuidv4()
+    profile = { ...profile, id: newId }
+    const newProfile = new Profile(profile)
+    newProfile.save()
+    res.status(200).json(profile);
+})
+
+app.post('/auth/login', async (req, res) => {
+    const profile = req.body;
+    const p = await Profile.findOne({ email: profile.email })
+    if (p === null) res.status(401)
+    if (p.password !== profile.password) {
+        res.status(401).send('Wrong Password')
+    } else {
+        res.status(200).json(p)
+    }
+})
+
+app.post('/get/tasks', async (req, res) => {
+    const user = req.body;
+    const tasks = await Task.find({ id: user.id })
+    res.status(200).send({ message: tasks })
+})
+
+app.post('/past', async (req, res) => {
+    const payload = req.body.payload;
+    const tasks = await PastTask.findOne({ id: payload.id })
+    if (!tasks) {
+        const pt = new PastTask(payload)
+        pt.save();
+        res.status(200);
+    } else {
+        res.status(300)
+    }
+
+})
+
+app.post('/get/pasttasks', async (req, res) => {
+    const user = req.body;
+    const tasks = await PastTask.find({ id: user.id })
+    res.status(200).send({ message: tasks })
+})
+
+app.post('/create-task', (req, res) => {
+    const body = req.body;
+    const newDate = body.date.split('T')[0]
+    const changedDate = new Date(newDate).getTime()
+    const newTask = new Task({
+        id: body.id,
+        title: body.title,
+        description: body.description,
+        due_date: changedDate
+    })
+    newTask.save()
+    res.status(200);
+    res.send({ message: "success" })
+})
+app.patch('/edit/task', async (req, res) => {
+    const body = req.body;
+    const changedDate = new Date(body.due_date).getTime()
+    const prevTask = await Task.updateOne({ id: body.id }, {
+        title: body.title,
+        description: body.description,
+        due_date: changedDate
+    })
+    console.log(prevTask.modifiedCount)
+    res.status(200).send("received!")
+})
+
+app.delete('/delete/task', async (req, res) => {
+    const body = req.body;
+    await Task.deleteOne({ _id: body.id })
+    res.status(200).send('Deleted!')
+})
+
+app.listen(5000, () => {
+    console.log('Server running at port 5000')
+})
